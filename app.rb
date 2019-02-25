11cc961491ffffeb1dfd0715396f6b282dc9fb9a
Christina Williams <christinaxtwilliams@gmail.com>
2019-02-25 20:55:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
+<<<<<<< HEAD
    "Started my server using Shotgun!"
   
+=======
+    "Welcome to your app!!!! I BUILT THIS!"
+>>>>>>> 9456bd91b9e8da7bf9b725629166f5d266b13fd1
   end
 
 end
